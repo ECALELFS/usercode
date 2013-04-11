1e46958387e3432546a47278aea55c995d637663
lpernie <>
2013-04-11 10:07:54 +0000

    *** empty log message ***

--- ECALpro/Pi0Analyzer/Analysis/Modules/test/RunOnBatch_Daemon.sh
+++ ECALpro/Pi0Analyzer/Analysis/Modules/test/RunOnBatch_Daemon.sh
@@ -4,7 +4,7 @@ cd /afs/cern.ch/work/l/lpernie/ECALpro/ETA_TRY/CMSSW_5_3_6/src/
 export SCRAM_ARCH=slc5_amd64_gcc462
 eval `scramv1 runtime -sh`
 cd Analysis/Modules/test
-#echo 'python /afs/cern.ch/work/l/lpernie/ECALpro/ETA_TRY/CMSSW_5_3_6/src/Analysis/Modules/test/RunOnBatch.py'
-#python /afs/cern.ch/work/l/lpernie/ECALpro/ETA_TRY/CMSSW_5_3_6/src/Analysis/Modules/testRunOnBatch.py
-echo 'python /afs/cern.ch/work/l/lpernie/ECALpro/ETA_TRY/CMSSW_5_3_6/src/Analysis/Modules/test/RunOnBatch_data.py'
-python /afs/cern.ch/work/l/lpernie/ECALpro/ETA_TRY/CMSSW_5_3_6/src/Analysis/Modules/test/RunOnBatch_data.py
+echo 'python /afs/cern.ch/work/l/lpernie/ECALpro/ETA_TRY/CMSSW_5_3_6/src/Analysis/Modules/test/RunOnBatch.py'
+python /afs/cern.ch/work/l/lpernie/ECALpro/ETA_TRY/CMSSW_5_3_6/src/Analysis/Modules/test/RunOnBatch.py
+#echo 'python /afs/cern.ch/work/l/lpernie/ECALpro/ETA_TRY/CMSSW_5_3_6/src/Analysis/Modules/test/RunOnBatch_data.py'
+#python /afs/cern.ch/work/l/lpernie/ECALpro/ETA_TRY/CMSSW_5_3_6/src/Analysis/Modules/test/RunOnBatch_data.py
